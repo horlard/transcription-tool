4b7050483931db6abee749e07fed4bf88e498167
horlard <oladipuposaulih@gmail.com>
2023-12-19 12:31:19 +0100

    update

--- src/app/api/whisper/index.tsx
+++ src/app/api/whisper/index.tsx
@@ -4,7 +4,7 @@ export const transcribeAudio = (data: FormData) => {
   return request.post("/transcriptions", data, {
     headers: {
       "Content-Type": "multipart/form-data",
-      Authorization: `Bearer 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`,
+      Authorization: `Bearer ${process.env.REACT_APP_OPENAI_KEY}`,
     },
   });
 };
