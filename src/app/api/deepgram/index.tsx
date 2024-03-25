6fc82527f24f6bb1e595f74da68ea20f7d7f9bbf
horlard <oladipuposaulih@gmail.com>
2024-03-25 22:10:01 +0100

    update

--- src/app/api/deepgram/index.tsx
+++ src/app/api/deepgram/index.tsx
@@ -1,9 +1,10 @@
 import request from "../request";
 
+const DEEPGRAM_KEY = process.env.REACT_APP_DEEPGRAM_KEY;
 export const transcribeAudio = (data: FormData) => {
   return request.post("/listen", data, {
     headers: {
-      Authorization: `Token ${process.env.REACT_APP_DEEPGRAM_KEY}`,
+      Authorization: `Token ${DEEPGRAM_KEY}`,
     },
     params: {
       model: "nova-2",
